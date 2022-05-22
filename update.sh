0d7065e71492c59960cb886c70cddd41b9bb1826
lustered <smellslikelavenders@gmail.com>
2022-05-22 12:48:18 -0400

    bspwm dot files

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+cp ~/.vimrc .
+cp ~/.tmux.conf .
+cp ~/.config/picom.conf config
+cp -r ~/.vim config
+cp -r ~/.config/bspwm config
+cp -r ~/.config/sxhkd config
+cp -r ~/.config/kitty/ config
+cp -r ~/.config/dunst config
+cp -r ~/.config/mpd config
+cp -r ~/.config/ncmpcpp config
+cp -r ~/.config/polybar/ config
